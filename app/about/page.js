1952eb6593ff77b9d039a3e7cbc571b696f8b4d7
jaiv24 <jaiv.maurya@gmail.com>
2025-04-07 23:57:41 -0600

    Enhance layout and navigation structure with header, footer, and About/Contact pages

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h1>About Us</h1>
+        <p>
+          Welcome to our Next.js application! This platform is designed to provide a seamless and modern web experience, showcasing the power of Next.js for building fast and scalable web applications.
+        </p>
+        <p>
+          Our mission is to deliver high-quality content and features that enhance user engagement and satisfaction. Whether you're exploring our feed, managing your profile, or learning more about us, we aim to make your experience enjoyable and intuitive.
+        </p>
+        <p>
+          Thank you for visiting, and we hope you enjoy using our application as much as we enjoyed building it!
+        </p>
+      </div>
+    );
+  }
---
+++ app/contact/page.js
@@ -0,0 +1,15 @@
+export default function Contact(){
+    return(
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h1>Contact Us</h1>
+        <p>This is the Contact page of the application.</p>
+        <br/>
+        <p>phone: +1 (555) 123-4567
+        <br/>
+         email : random@me.com
+        </p>
+
+        </div>
+       
+    );
+}
